3ac85adab51c64d2274707bd71e2977b85625a6f
liuchengyong <980527632@qq.com>
2025-02-06 15:14:24 +0800

    修改

---
+++ src/app/(web)/page.tsx
@@ -0,0 +1,27 @@
+import { Layout, Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+
+const siderStyle: React.CSSProperties = {
+    overflow: 'auto',
+    height: '100vh',
+    position: 'sticky',
+    insetInlineStart: 0,
+    top: 0,
+    bottom: 0,
+    scrollbarWidth: 'thin',
+    scrollbarGutter: 'stable',
+  };
+
+
+export default async function Guide() {
+    return <Layout hasSider>
+        <Sider style={siderStyle}>
+            <div className="demo-logo-vertical" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={[{
+                key: 'dasdas',
+                label: 'ceshi',
+            }]} />
+        </Sider>
+
+    </Layout>;
+}
